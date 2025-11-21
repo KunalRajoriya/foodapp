a125c574ee29f21614421ec9a97cc069ce9e5281
KUNAL RAJORIYA <kunalrajoriya77@gmail.com>
2025-11-21 11:29:01 +0530

    API starts returning video URL

--- backend/src/controller/food.controller.js
+++ backend/src/controller/food.controller.js
@@ -1,17 +1,26 @@
 const foodModel = require('../models/food.model');
 const storageService = require('../services/storage.service');
+const { v4: uuid} = require("uuid")
 
 async function createFood(req, res) {
 
-  console.log(req.foodPartner)
-
-  console.log(req.body)
-  console.log(req.file)
-
   const fileUploadResult = await storageService.uploadFile(
     req.file.buffer,
-    req.file.originalname
+    uuid()
   );
+
+  const foodItem = await foodModel.create({
+    name: req.body.name,
+    description: req.body.description,
+    video: fileUploadResult.url,
+    foodpartner: req.foodPartner._id,
+  })
+
+  res.status(201).json({
+    message:"food created succesfully",
+    food: foodItem,
+
+  })
 
   res.send("food item created") 
 
